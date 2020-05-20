48230f6a6df62ecf8c8b3281ac3a5fecb0f0508b
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-05-20 19:03:31 +0900

    migrate_post-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_162110) do
+ActiveRecord::Schema.define(version: 2020_05_20_094858) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "title", null: false
+    t.text "text"
+    t.integer "price", null: false
+    t.integer "people", null: false
+    t.string "url", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
